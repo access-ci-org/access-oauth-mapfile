f91ffad88fc535379b58fa1985fe68271917a083
Eric Blau <blau@anl.gov>
2020-10-15 20:31:25 +0000

    Initial commit of files extracted from xsede_user_mapfile-1.0.2.noarch.rpm

---
+++ mapfileupdate.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# The real map_file for Globus-ssh will be placed at MAP_FILE
+MAP_FILE=/etc/grid-security/xsede-user-mapfile
+# The python process for generating the MAP_FILE runs from this directory:
+MAP_FILE_GEN=/usr/local/share/utils/xsede_user_mapfile
+# convenience for the XCI RACD test team :
+# MAP_FILE_GEN=./
+
+if [ -d $MAP_FILE_GEN ]
+then
+	cd $MAP_FILE_GEN
+else
+	# go ahead and linux throw the error
+	cd $MAP_FILE_GEN
+	exit
+fi
+cd $MAP_FILE_GEN
+if [ -f ./GENMAP_IN_PROGRESS ]
+then
+	echo "A map_file generations process is running... exiting"
+	exit
+fi
+
+# do the update work
+touch ./GENMAP_IN_PROGRESS
+./xsede-user-mapfile.py
+
+# Try to keep the updates of $MAP_FILE somewhat atomic with cp 
+# leveraging linux kernel write buffering
+cp mymapfile $MAP_FILE
+# or alternatively, if you have entries in a master copy map file for
+# special cases (multiple local user names, vendor accounts...), 
+# you may want to copy and append to that file
+#
+# cp /pathto/mastermapfile $MAP_FILE; cat mymapfile >> $MAP_FILE
+
+rm ./GENMAP_IN_PROGRESS
+
---
+++ xsede-user-mapfile.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+"""
+Generate a Globus auth compatible map_file for use with Globus SSH
+
+See: https://github.com/XSEDE/oauth-ssh/blob/master/server/README.md 
+for a description of the map_file and discussion of how it may be used.
+
+Galen Arnold, XSEDE,  July 2019
+"""
+
+# To setup XA-API-KEY in the .json config file, see:
+# https://xsede-xdcdb-api.xsede.org  , and all the tabs there, especially:
+#  -> Request Headers
+#  -> Generate API-KEY
+
+# here to keep pylint happy with print()
+from __future__ import print_function
+import json
+import re
+import requests
+MYCONFIGFILE="xsede-user-mapfile-config.json"
+MYMAPFILE="mymapfile"
+
+
+def read_configuration(config_file):
+    """
+    Read the configuration file and return a tuple of the values.
+    """
+    print("reading config_file:", config_file)
+    myconfig = json.loads(config_file.read())
+    xa_agent = myconfig['XA-AGENT']
+    xa_resource = myconfig['XA-RESOURCE']
+    xa_api_key = myconfig['XA-API-KEY']
+    return (xa_agent, xa_resource, xa_api_key)
+
+
+def auth_test(xa_agent, xa_resource, xa_api_key):
+    """
+    Check the auth_test and json handling via requests
+    """
+    print("testing authentication, should return 200...")
+    # myresult = requests.get('https://xsede-xdcdb-api.xsede.org/userinfo/auth_test',
+
+    myresult = requests.get(
+        'https://a3mdev.xsede.org/xdcdb-api-test/userinfo/auth_test',
+        headers={'XA-AGENT':xa_agent,
+                 'XA-RESOURCE':xa_resource,
+                 'XA-API-KEY':xa_api_key})
+    print(myresult, "\n")
+    if (not(re.search("200",str(myresult),flags=0))):
+        print ("Authentication with xdcdb-api-test failed.\n")
+        exit()
+    
+
+def gen_mapfile(xa_agent, xa_resource, xa_api_key, output_file):
+    """
+    Make the real query and generate a mapfile
+    """
+    print("generating {} for:".format(output_file))
+    myurl = "https://a3mdev.xsede.org/"
+    myurl += "xdcdb-api-test/userinfo/v1/resources/usernames/"
+    myurl += xa_resource
+    myresult = requests.get(myurl,
+                            headers={'XA-AGENT':xa_agent,
+                                     'XA-RESOURCE':xa_resource,
+                                     'XA-API-KEY':xa_api_key})
+    mydata = myresult.json()
+
+    print("   XA-RESOURCE: {}".format(mydata['result']['resource']))
+    print("\nThe following entries mapping to multiple local accounts")
+    print("\"portalLogin localaccta localacctb\" WERE written to {}.\n".format(MYMAPFILE))
+  
+    i = 0
+    for somebody in mydata['result']['users']:
+        # if there are multiple usernames,
+        # make a note of it but don't write to mapfile
+        if len(somebody['usernames']) > 1:
+             multilocalstring = somebody['portalLogin']
+             for name in somebody['usernames']:
+                 multilocalstring += " " + name
+                 output_file.write("{}@xsede.org {}\n".
+                                   format(somebody['portalLogin'],
+                                          name))
+             print(multilocalstring)
+        else:
+            output_file.write("{}@xsede.org {}\n".
+                              format(somebody['portalLogin'],
+                                     somebody['usernames'][0]))
+            i = i + 1
+    print("\nlines written to {}: {}\n".format(MYMAPFILE,i))
+
+
+def main():
+    """
+    main program
+
+    """
+    config_file = open(MYCONFIGFILE, "r")
+    (xa_agent, xa_resource, xa_api_key) = read_configuration(config_file)
+    config_file.close()
+
+    auth_test(xa_agent, xa_resource, xa_api_key)
+
+    output_file = open(MYMAPFILE, "w")
+    gen_mapfile(xa_agent, xa_resource, xa_api_key, output_file)
+    output_file.close()
+
+
+main()
