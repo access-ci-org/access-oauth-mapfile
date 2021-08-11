1c636e1e769b0af9e3946a924695e6d02bf09e82
vagrant <vagrant@localhost.localdomain>
2021-08-11 21:48:41 +0000

    - Add command line options to write to stdout, and to specify what resource to look up.  Command line overrides configuration file.   - Add CONFIG_FILE definition in xsede-oauth-mapfile.sh (defaults to etc/xsede-oauth-mapfile-config.json), and invokes xsede-oauth-mapfile.py with the config option.  This allows a site to make modified copies of the shell script to create mapfiles for different resources in cron.

--- bin/xsede-oauth-mapfile.sh
+++ bin/xsede-oauth-mapfile.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# The base install directory where mapfile generation is run from
-MAP_FILE_BASE=/usr/local/share/utils/xsede_oauth_mapfile
-# The mapfile generated from XDCDB in the current working directory
-GEN_FILE=xsede-oauth-mapfile
-# Other locally managed mapfile entries
-LOCAL_FILE=/etc/grid-security/xsede-oauth-mapfile.local
 # The real map_file used by services
 MAP_FILE=/etc/grid-security/xsede-oauth-mapfile
+# The config file used by this invokation of the xsede-oauth-mapfile script
+CONFIG_FILE=etc/xsede-oauth-mapfile-config.json
+# The mapfile generated from XDCDB in the current working directory
+GEN_FILE=xsede-oauth-mapfile
+# The base install directory where mapfile generation is run from
+MAP_FILE_BASE=/usr/local/share/utils/xsede_oauth_mapfile
+# Other locally managed mapfile entries
+LOCAL_FILE=/etc/grid-security/xsede-oauth-mapfile.local
 # The lockfile
-MAP_LOCK=./GEN_MAP.LOCK
+#MAP_LOCK=./GEN_MAP.LOCK
+MAP_LOCK=./${GEN_FILE}.LOCK
+echo $MAP_LOCK
 
 if [ -d $MAP_FILE_BASE ]
 then
@@ -28,7 +32,7 @@ fi
 
 # Do the update work
 touch $MAP_LOCK
-./bin/xsede-oauth-mapfile.py -m $GEN_FILE
+./bin/xsede-oauth-mapfile.py -m $GEN_FILE -c $CONFIG_FILE
 
 # If you have local mappings place them in $LOCAL_FILE
 # Append local mappings to the generated file
